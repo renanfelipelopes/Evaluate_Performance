a4d96dcfa58d718cd2ddada07bb14194d8def630
Renan Lopes <renanlopes.ads@gmail.com>
2021-11-02 21:04:06 -0300

    start exercise

---
+++ Evaluate_Performance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Evaluate_Performance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
