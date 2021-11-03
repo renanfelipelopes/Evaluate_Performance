63c3e01f22873adad6f7ce3afb1c09a27418e4e3
Renan Lopes <renanlopes.ads@gmail.com>
2021-11-02 21:38:57 -0300

    final

--- Evaluate_Performance/Program.cs
+++ Evaluate_Performance/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using Evaluate_Performance.Entities;
+using Evaluate_Performance.Services;
 
 namespace Evaluate_Performance
 {
@@ -6,7 +10,25 @@ namespace Evaluate_Performance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(vect[0], price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product p = calculationService.Max(list);
+
+            Console.WriteLine();
+            Console.WriteLine("Most expansive:");
+            Console.WriteLine(p);
         }
     }
 }
